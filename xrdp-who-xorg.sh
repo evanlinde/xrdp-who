89b128a70ff8f95e51f4cd2e9a583b63a77895d5
Evan Linde <elinde@okstate.edu>
2022-07-26 22:44:42 -0500

    reduce external program calls

--- xrdp-who-xorg.sh
+++ xrdp-who-xorg.sh
@@ -17,11 +17,11 @@ _printf="%7s %-20s %-19s %-12s\n"
 # Print header
 printf "\n${_printf}" PID USERNAME START_TIME STATUS
 
-ps h -C Xorg -o user:20,pid,lstart,cmd | grep xrdp | while read _ps; do
-    timestring=$(echo ${_ps} | awk '{print $3,$4,$5,$6,$7}');
-    start_time=$(date -d "${timestring}" +"%Y-%m-%d %H:%M:%S");
-    [ $(date -d "${start_time}" +%s) -lt $(date -d "-30 days" +%s) ] && start_time="${YELLOW}${start_time}${ENDCOLOR}"
-    read username pid <<< $(echo ${_ps} | awk '{print $1,$2}');
+ps h -C Xorg -o user:20,pid,lstart,cmd | grep xrdp | while read username pid dt1 dt2 dt3 dt4 dt5 xorg_cmd; do
+    timestring="${dt1} ${dt2} ${dt3} ${dt4} ${dt5}";
+    start_time_s=$(date -d "${timestring}" +"%s");
+    printf -v start_time "%(%Y-%m-%d %H:%M)T" ${start_time_s}
+    [ ${start_time_s} -lt $(date -d "-30 days" +%s) ] && start_time="${YELLOW}${start_time}${ENDCOLOR}"
     ss -ep 2>/dev/null | grep '/xrdp_display' | grep -q pid\=${pid}, && status="${GREEN}active${ENDCOLOR}" || status="${RED}disconnected${ENDCOLOR}";
     printf "${_printf}" ${pid} ${username} "${start_time}" "${status}";
 done
--- xrdp-who-xvnc.sh
+++ xrdp-who-xvnc.sh
@@ -17,12 +17,13 @@ _printf="%7s %-20s %-19s %-10s %4s %-12s\n"
 # Print header
 printf "\n${_printf}" PID USERNAME START_TIME GEOMETRY BITS STATUS
 
-ps h -C Xvnc -o user:20,pid,lstart,cmd | while read _ps; do
-	timestring=$(echo ${_ps} | awk '{print $3,$4,$5,$6,$7}');
-	start_time=$(date -d "${timestring}" +"%Y-%m-%d %H:%M:%S");
-	[ $(date -d "${start_time}" +%s) -lt $(date -d "-30 days" +%s) ] && start_time="${YELLOW}${start_time}${ENDCOLOR}"
-	read username pid geometry colorbits <<< $(echo ${_ps} | awk '{user=$1; pid=$2; for(i=8;i<=NF;i++){if($i=="-geometry"){geom=$(++i)} if($i=="-depth"){bits=$(++i)}} print user,pid,geom,bits}');
-	ss -tep 2>/dev/null | grep -q pid\=${pid}, && status="${GREEN}active${ENDCOLOR}" || status="${RED}disconnected${ENDCOLOR}";
-	printf "${_printf}" ${pid} ${username} "${start_time}" ${geometry} ${colorbits} "${status}";
+ps h -C Xvnc -o user:20,pid,lstart,cmd | while read username pid dt1 dt2 dt3 dt4 dt5 xvnc_cmd; do
+    timestring="${dt1} ${dt2} ${dt3} ${dt4} ${dt5}";
+    start_time_s=$(date -d "${timestring}" +"%s");
+    printf -v start_time "%(%Y-%m-%d %H:%M)T" ${start_time_s}
+    [ ${start_time_s} -lt $(date -d "-30 days" +%s) ] && start_time="${YELLOW}${start_time}${ENDCOLOR}"
+    read geometry colorbits <<< $(echo ${xvnc_cmd} | awk '{for(i=i;i<=NF;i++){if($i=="-geometry"){geom=$(++i)} if($i=="-depth"){bits=$(++i)}} print geom,bits}');
+    ss -tep 2>/dev/null | grep -q pid\=${pid}, && status="${GREEN}active${ENDCOLOR}" || status="${RED}disconnected${ENDCOLOR}";
+    printf "${_printf}" ${pid} ${username} "${start_time}" ${geometry} ${colorbits} "${status}";
 done
 echo ""
